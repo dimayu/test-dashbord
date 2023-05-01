9c028e5851adca193df303dc51dde4290ee98904
damir <dimayu82@gmail.com>
2023-05-01 14:41:20 +0300

    html markup

---
+++ src/blocks/modules/list-header/list-header.js
@@ -0,0 +1,13 @@
+modules.define('list-header', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
